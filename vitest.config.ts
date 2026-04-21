70588b4417f33695980ac9162117bcc6a87c92f6
davidcorne <davidcorne@gmail.com>
2026-04-21 23:25:00 +0100

    Sync the svelte-kit

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vitest/config';
+import { resolve } from 'path';
 
 export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: [],
+    include: ['src/**/__tests__/**/*.test.ts'],
   },
 });
